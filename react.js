fd3d957b112b8f7550dd23529c567a0ed20cac5a
Martynas Žilinskas <martynas@quatrodev.com>
2020-08-13 14:31:16 +0300

    Feature: Updated rules and dependencies. (#14)
    * Updated rules.
    
    * Updated dependencies.

--- react.js
+++ react.js
@@ -7,22 +7,6 @@ module.exports = {
         }
     },
     rules: {
-        //#region Import rules.
-        "import/order": [
-            "warn",
-            {
-                groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
-                pathGroups: [
-                    {
-                        pattern: "./*.scss",
-                        group: "index",
-                        position: "after"
-                    }
-                ]
-            }
-        ],
-        //#endregion
-
         "react/jsx-uses-react": "warn",
         "react/jsx-uses-vars": "error",
         "react/jsx-key": "error",
@@ -32,6 +16,7 @@ module.exports = {
         "react/button-has-type": "warn",
         "react/no-string-refs": "error",
         "react/jsx-boolean-value": ["warn", "never"],
-        "prettier/prettier": "warn"
+        "prettier/prettier": "warn",
+        "import/no-default-export": "off"
     }
 };
